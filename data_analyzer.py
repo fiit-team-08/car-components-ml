327eb3a9fb31d707bae7992a5536fcc75ee0eae3
Roman <roman.slonsky@taphome.com>
2020-10-21 13:23:38 +0200

    read new datasets

--- data_analyzer.py
+++ data_analyzer.py
@@ -3,20 +3,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 from similaritymeasures import area_between_two_curves, curve_length_measure, frechet_dist
 
-def init_dataframe(path):
+def init_dataframe_old(path):
     df = pd.read_csv(path, header=None, sep=';')
     df = df.drop(columns=[0, 1, 3, 5, 7, 9, 11, 13, 15, 17])
     df.columns = ['LAT', 'LON', 'UTMX', 'UTMY', 'HMSL', 'GSPEED', 'CRS', 'HACC', 'NXPT']
     return df
 
-df0 = init_dataframe('data/feri_logy_analyza/200629_karting/200629130554_gps.log')
-df1 = init_dataframe('data/feri_logy_analyza/200629_karting/kartfinal36pr3r.csv')
-df2 = init_dataframe('data/200623_academy/200623110845_gps.log')
-df3 = init_dataframe('data/200623_academy/200623111152_gps.log')
-df4 = init_dataframe('data/200623_academy/200623120513_gps.log')
-df5 = init_dataframe('data/200623_academy/200623121213_gps.log')
-df6 = init_dataframe('data/200623_academy/200623122206_gps.log')
-df7 = init_dataframe('data/200623_academy/trasa23.csv')
+def init_dataframe(path):
+    df = pd.read_csv(path, sep=',')
+    return df
+
+df0 = init_dataframe_old('data/feri_logy_analyza/200629_karting/200629130554_gps.log')
+df1 = init_dataframe_old('data/feri_logy_analyza/200629_karting/kartfinal36pr3r.csv')
+df2 = init_dataframe_old('data/200623_academy/200623110845_gps.log')
+df3 = init_dataframe_old('data/200623_academy/200623111152_gps.log')
+df4 = init_dataframe_old('data/200623_academy/200623120513_gps.log')
+df5 = init_dataframe_old('data/200623_academy/200623121213_gps.log')
+df6 = init_dataframe_old('data/200623_academy/200623122206_gps.log')
+df7 = init_dataframe_old('data/200623_academy/trasa23.csv')
+
+ref1 = init_dataframe('data/ref1.csv')
+ref2 = init_dataframe('data/ref2.csv')
+traces1 = init_dataframe('data/traces1.csv')
+traces2 = init_dataframe('data/traces2.csv')
+
+# plt.show()
 
 df0.plot(x='LAT', y='LON')
 df1.plot(x='LAT', y='LON')
@@ -40,14 +51,14 @@ def create_curve(dataframe):
     curve[:, 1] = dataframe.LAT / 10**6
     return curve
 
-experimental_curve = create_curve(df7)
-ideal_curve = create_curve(df7)
+# experimental_curve = create_curve(df7)
+# ideal_curve = create_curve(df7)
 
-fd = frechet_dist(experimental_curve, ideal_curve)
-print(fd)
+# fd = frechet_dist(experimental_curve, ideal_curve)
+# print(fd)
 
-cl = curve_length_measure(experimental_curve, ideal_curve)
-print(cl)
+# cl = curve_length_measure(experimental_curve, ideal_curve)
+# print(cl)
 
 
 # fig = plt.figure()
