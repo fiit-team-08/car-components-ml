264a65b9efcc08f2491a2014b7793ccb4063c6b3
Roman <roman.slonsky@taphome.com>
2020-10-26 22:35:58 +0100

    Created differences for every lap according to reference tracks

--- data_analyzer.py
+++ data_analyzer.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from similaritymeasures import area_between_two_curves, curve_length_measure, frechet_dist
+from os import listdir
 
 def init_dataframe_old(path):
     df = pd.read_csv(path, header=None, sep=';')
@@ -13,46 +14,68 @@ def init_dataframe(path):
     df = pd.read_csv(path, sep=',')
     return df
 
-df0 = init_dataframe_old('data/feri_logy_analyza/200629_karting/200629130554_gps.log')
-df1 = init_dataframe_old('data/feri_logy_analyza/200629_karting/kartfinal36pr3r.csv')
-df2 = init_dataframe_old('data/200623_academy/200623110845_gps.log')
-df3 = init_dataframe_old('data/200623_academy/200623111152_gps.log')
-df4 = init_dataframe_old('data/200623_academy/200623120513_gps.log')
-df5 = init_dataframe_old('data/200623_academy/200623121213_gps.log')
-df6 = init_dataframe_old('data/200623_academy/200623122206_gps.log')
-df7 = init_dataframe_old('data/200623_academy/trasa23.csv')
+# df0 = init_dataframe_old('data/feri_logy_analyza/200629_karting/200629130554_gps.log')
+# df1 = init_dataframe_old('data/feri_logy_analyza/200629_karting/kartfinal36pr3r.csv')
+# df2 = init_dataframe_old('data/200623_academy/200623110845_gps.log')
+# df3 = init_dataframe_old('data/200623_academy/200623111152_gps.log')
+# df4 = init_dataframe_old('data/200623_academy/200623120513_gps.log')
+# df5 = init_dataframe_old('data/200623_academy/200623121213_gps.log')
+# df6 = init_dataframe_old('data/200623_academy/200623122206_gps.log')
+# df7 = init_dataframe_old('data/200623_academy/trasa23.csv')
+
+# df0.plot(x='LAT', y='LON')
+# df1.plot(x='LAT', y='LON')
+# df2.plot(x='LAT', y='LON')
+# df3.plot(x='LAT', y='LON')
+# df4.plot(x='LAT', y='LON')
+# df5.plot(x='LAT', y='LON')
+# df6.plot(x='LAT', y='LON')
+# df7.plot(x='LAT', y='LON')
 
 ref1 = init_dataframe('data/ref1.csv')
 ref2 = init_dataframe('data/ref2.csv')
-traces1 = init_dataframe('data/traces1.csv')
-traces2 = init_dataframe('data/traces2.csv')
+# traces1 = init_dataframe('data/traces1.csv')
+# traces2_1 = init_dataframe('data/traces2-1.csv')
+# traces2_2 = init_dataframe('data/traces2-2.csv')
 
+# traces2_1.plot(x='LAT', y='LON')
+# traces2_2.plot(x='LAT', y='LON')
 # plt.show()
 
-df0.plot(x='LAT', y='LON')
-df1.plot(x='LAT', y='LON')
-df2.plot(x='LAT', y='LON')
-df3.plot(x='LAT', y='LON')
-df4.plot(x='LAT', y='LON')
-df5.plot(x='LAT', y='LON')
-df6.plot(x='LAT', y='LON')
-df7.plot(x='LAT', y='LON')
-
-# plt.show()
-
-fig = plt.figure()
-for frame in [df2, df7]:
-    plt.plot(frame['LON'], frame['LAT'])
+# fig = plt.figure()
+# for frame in [df2, df7]:
+#     plt.plot(frame['LON'], frame['LAT'])
 #plt.show()
 
 def create_curve(dataframe):
     curve = np.zeros((dataframe.shape[0], 2))
-    curve[:, 0] = dataframe.LON / 10**6
-    curve[:, 1] = dataframe.LAT / 10**6
+    curve[:, 0] = dataframe.LON
+    curve[:, 1] = dataframe.LAT
     return curve
 
-# experimental_curve = create_curve(df7)
-# ideal_curve = create_curve(df7)
+ideal_curve_ref1 = create_curve(ref1)
+ideal_curve_ref2 = create_curve(ref2)
+laps_dir = 'data/laps'
+
+nameColumn = 'Name'
+frechetColumn = 'Frechet distance'
+curveLenColumn = 'Curve length measure'
+data_structure = {nameColumn: [], frechetColumn: [], curveLenColumn: []}
+difference_df = pd.DataFrame(data=data_structure)
+
+for fileName in listdir(laps_dir):
+    lap = init_dataframe('{}/{}'.format(laps_dir, fileName))
+    experimental_curve = create_curve(lap)
+
+    ideal_curve = ideal_curve_ref1 if fileName.startswith('lap1') else ideal_curve_ref2
+    fd = frechet_dist(experimental_curve, ideal_curve)
+    cl = curve_length_measure(experimental_curve, ideal_curve)    
+
+    difference_df = difference_df.append({nameColumn: fileName, frechetColumn: fd, curveLenColumn: cl}, ignore_index=True)
+    print(fileName)
+
+difference_df.to_csv('data/differences.csv', index=False)
+plt.show()
 
 # fd = frechet_dist(experimental_curve, ideal_curve)
 # print(fd)
