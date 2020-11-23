5f815afc6b3bd8c32f3b2f4e204cee9ed2c3191d
Katka <katkajuhasova8@gmail.com>
2020-11-23 22:10:20 +0100

    Add #69 environment for RL

--- stanley_controller/stanley.py
+++ stanley_controller/stanley.py
@@ -7,23 +7,23 @@ https://ch.mathworks.com/help/matlab/matlab_external/install-the-matlab-engine-f
 from matlab.engine import start_matlab
 from matlab import double
 import math
-import io 
+import io
+
 
 class MatlabWrapper:
 
-    def __init__(self):    
+    def __init__(self):
         self._out = io.StringIO()
         self._err = io.StringIO()
         self._eng = start_matlab()
-
 
     def __del__(self):
         self._eng.quit()
         self._out.close()
         self._err.close()
 
-
-    def steering_command(self, ref_pose: list, curr_pose: list, velocity: float):
+    def steering_command(self, ref_pose: list, curr_pose: list,
+                         velocity: float):
         """
         Computes the steering angle command steerCmd, in degrees, using the  
         Stanley method.
@@ -48,13 +48,16 @@ class MatlabWrapper:
 
         ref_pose = double(ref_pose)
         curr_pose = double(curr_pose)
-        cmd = self._eng.lateralControllerStanley(ref_pose, curr_pose, velocity,
-            stdout=self._out, stderr=self._err)
+        cmd = self._eng.lateralControllerStanley(
+            ref_pose, curr_pose, velocity,
+            stdout=self._out, stderr=self._err
+        )
+
         self._log()
+
         return cmd
 
-
-    def bicycle_kinematic_model(self, steering_angle = math.pi/4):
+    def bicycle_kinematic_model_v0(self, steering_angle=math.pi/4):
         """
         Creates a bicycle vehicle model to simulate simplified car-like vehicle dynamics. 
         This model represents a vehicle with two axles separated by a distance, WheelBase. 
@@ -76,11 +79,15 @@ class MatlabWrapper:
             Kinematic model of a bicycle. 
         """
 
-        bicycle = self._eng.bicycleKinematics("MaxSteeringAngle", steering_angle, stdout=self._out, stderr=self._err)
+        bicycle = self._eng.bicycleKinematics(
+            "MaxSteeringAngle", steering_angle, stdout=self._out,
+            stderr=self._err
+        )
+
         self._log()
+
         return bicycle
 
-    
     def _log(self):
         output = self._out.getvalue()
         if output != '':
@@ -90,3 +97,45 @@ class MatlabWrapper:
         if error != '':
             print(error)
 
+    def bicycle_kinematic_model(self, x: float, y: float, theta: float,
+                                v: float, psi: float) -> list:
+        """
+        Creates a bicycle vehicle model to simulate simplified car-like vehicle dynamics.
+        This model represents a vehicle with two axles separated by a distance, WheelBase.
+        The state of the vehicle is defined as a three-element vector, [x y theta], with a global xy-position,
+        specified in meters, and a vehicle heading angle, theta, specified in radians. The front wheel can be turned with steering angle psi.
+        The vehicle heading, theta, is defined at the center of the rear axle.
+        To compute the time derivative states of the model, use the derivative function with input commands and the current robot state.
+
+        Parameters
+        ----------
+            x : float
+                Global x coordinate in meters.
+            y : float
+                Global y coordinate in meters.
+            theta : float
+                Vehicle heading angle specified in radians.
+            v : float
+                Velocity of the vehicle in m/s.
+            psi : float
+                The front wheel steering angle specified in radians.
+
+        Returns
+        ----------
+            New global state of the vehicle as a list [x, y, theta].
+        """
+
+        self._eng.eval('''
+        kinematicModel = bicycleKinematics;
+        state = [{} {} {}];
+        tspan = 0:0.05:0.1;
+        inputs = [{} {}];
+        [t,y] = ode45(@(t,y)derivative(kinematicModel,y,inputs),tspan,state);
+        '''.format(x, y, theta, v, psi),
+                       nargout=0,
+                       stdout=self._out,
+                       stderr=self._err
+        )
+        new_state = self._eng.workspace['y'][-1]
+
+        return new_state
