a55e2efc0a414867d4a0a5494a6092536cd5ffed
Roman <roman.slonsky@taphome.com>
2020-12-02 14:57:48 +0100

    task107: implemented change_state method.

--- RL/bicycle_model/bicycle_model.py
+++ RL/bicycle_model/bicycle_model.py
@@ -1,15 +1,32 @@
+from math import cos, sin, tan
+
 class BicycleKinematicModel:
     
-    def __init__(self, x, y, steering_angle, heading_angle):
-        self._x = x
-        self._y = y
-        self._steering_angle = steering_angle
-        self._heading_angle = heading_angle
+    def __init__(self, x, y, steering_angle, heading_angle, bicycle_len = 1.5):
+        self._x = x                                 # meters
+        self._y = y                                 # meters
+        self._steering_angle = steering_angle       # radians
+        self._heading_angle = heading_angle         # radians
+        self.bicycle_len = bicycle_len              # meters
 
     def get_state(self):
         return self._x, self._y, self._steering_angle, self._heading_angle
 
-    def change_state(self):
-        pass
+    def change_state(self, velosity, steering_rate):
+        R = self.bicycle_len / tan(self._steering_angle)    
+        rotation_rate = velosity / R
+
+        delta_x = velosity * cos(self._heading_angle)
+        delta_y = velosity * sin(self._heading_angle)
+        delta_theta = rotation_rate 
+        delta_sigma = steering_rate
+
+        self._x += delta_x
+        self._y += delta_y
+        self._heading_angle += delta_theta
+        self._steering_angle += delta_sigma
+    
+
+        
 
     
